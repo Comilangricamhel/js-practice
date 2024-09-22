2444345a58de65b86f55a4343d0053be4b9aac83
Comilangricamhel <carolcomilang06@gmail.com>
2024-09-23 07:47:11 +0800

    Create Activity 1.js

---
+++ Activity 1.js
@@ -0,0 +1,16 @@
+// Vvariabe to store my name
+let name = "Rica Mhel";
+
+// Variable to store ur life goal
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+// Variable to store my gender
+let gender = "Famale";
+
+// Variable to store my Facebook account name
+let facebookAcct = "Rica Dump";
+
+console.log("Name:", name);
+console.log("Life Goal:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Facebook Account:", facebookAcct);
