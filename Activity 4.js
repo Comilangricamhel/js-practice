f638798f2aa52cded1232173857a26121781be36
Comilangricamhel <carolcomilang06@gmail.com>
2024-09-23 07:51:09 +0800

    Create Activity 4.js

---
+++ Activity 4.js
@@ -0,0 +1,21 @@
+// My Favourite  number
+let firstFavNumb = 3; 
+let secondFavNumb = 7;
+
+let isGreater = firstFavNumb > secondFavNumb;
+let isLess = firstFavNumb < secondFavNumb;
+let isGreaterOrEqual = firstFavNumb >= secondFavNumb;
+let isLessOrEqual = firstFavNumb <= secondFavNumb;
+let isStrictEqual = firstFavNumb === secondFavNumb;
+let isLooseEqual = firstFavNumb == secondFavNumb;
+let isStrictNotEqual = firstFavNumb !== secondFavNumb;
+let isLooseNotEqual = firstFavNumb != secondFavNumb;
+
+console.log("Is firstFavNumb greater than secondFavNumb?:", isGreater);
+console.log("Is firstFavNumb less than secondFavNumb?: ", isLess);
+console.log("Is firstFavNumb greater than or equal to secondFavNumb?:", isGreaterOrEqual);
+console.log("Is firstFavNumb less than or equal to secondFavNumb?:", isLessOrEqual);
+console.log("Is firstFavNumb strictly equal to secondFavNumb?:", isStrictEqual);
+console.log("Is firstFavNumb loosely equal to secondFavNumb?: ", isLooseEqual);
+console.log("Is firstFavNumb strictly not equal to secondFavNumb?:", isStrictNotEqual);
+console.log("firstFavNumb loosely not equal to secondFavNumb?:", isLooseNotEqual);
