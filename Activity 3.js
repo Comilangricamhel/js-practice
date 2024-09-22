1be9de33fdc0db63629ca8444ebcefe07da033b2
Comilangricamhel <carolcomilang06@gmail.com>
2024-09-23 07:49:40 +0800

    Create Activity 3.js

---
+++ Activity 3.js
@@ -0,0 +1,14 @@
+let isJsProgrammingLanguage = true; // 1. Create  a vriable to store true for JavaScript being a programming language
+
+// 2. Create a variable  to store false for JavaScript being hard
+let isJsHard = false;
+
+// 3. Create a variable to store your favorite number
+let favNumb = 4;
+let result = favNumb + undefined;
+
+
+console.log("Is JavaScript a programming language?:", isJsProgrammingLanguage);
+console.log("Is JavaScript hard?:", isJsHard, '\n');
+console.log("Favorite Number + undefined:", result);
+console.log("FavNumb:", favNumb);
