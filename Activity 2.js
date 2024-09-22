4f4a935015622c6eeb6065aa27c8d789742642a0
Comilangricamhel <carolcomilang06@gmail.com>
2024-09-23 07:48:41 +0800

    Create Activity 2.js

---
+++ Activity 2.js
@@ -0,0 +1,15 @@
+let firstFavNum = 4; 
+let secondFavNum = 6;
+let additionResult = firstFavNum + secondFavNum;
+let subtractionResult = firstFavNum - secondFavNum;
+let multiplicationResult = firstFavNum * secondFavNum;
+let divisionResult = firstFavNum / secondFavNum;
+let modulusResult = firstFavNum % secondFavNum;
+let powerResult = firstFavNum ** secondFavNum;
+
+console.log("Addition: " + additionResult);
+console.log("Subtraction: " + subtractionResult);
+console.log("Multiplication: " + multiplicationResult);
+console.log("Division: " + divisionResult);
+console.log("Modulus: " + modulusResult);
+console.log("Power: " + powerResult);
