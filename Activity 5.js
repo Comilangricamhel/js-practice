a8955290d283456671e45cfe1aaf3cbcb338ceac
Comilangricamhel <carolcomilang06@gmail.com>
2024-09-23 07:54:06 +0800

    Create Activity 5.js

---
+++ Activity 5.js
@@ -0,0 +1,21 @@
+// My favourite Actor name
+let myFavActorFirstName = "Dave";
+let myFavActorLastName = "Richard";
+
+// His fullname
+let actorFullName = myFavActorFirstName + " " + myFavActorLastName;
+
+// Capitalize the full name
+let uppercase = actorFullName.toUpperCase();
+
+// Message
+let message = ' My favorite Actor is '+ uppercase + ' cuz he is tactical in fights.';
+
+// This Append message that his best show is Silicon Valley
+message += " His best show is Silicon Valley.";
+
+console.log("First Name:", myFavActorFirstName);
+console.log("Last Name:", myFavActorLastName);
+console.log("Full name:", actorFullName);
+console.log("Uppercase of his full name:", uppercase);
+console.log("\tMessage:", message);
